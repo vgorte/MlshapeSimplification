dc3d3dec1e2998f668f2a0a3c2706e9e342eb237
rkans02 <rkans02@users.noreply.github.com>
2019-06-19 15:14:19 +0200

    Code Cleaning
    Removed logs and empty spaces

--- Scripts/AngleFinder.py
+++ Scripts/AngleFinder.py
@@ -1,31 +1,24 @@
-import sys
 import math
-import os, random
 import numpy as np
-import matplotlib.pyplot as plt
 
 def computeLength(P1, P2):    
     x1 = P1[0]
     y1 = P1[1]
     x2 = P2[0]
     y2 = P2[1]
-    
     a = math.pow( (x1 - x2) , 2 )
     b = math.pow( (y1 - y2) , 2 )
-    
     length = math.sqrt( a + b )
     return length
 
 def computeAngle(first, second, third):
     P12 = computeLength(first, second)
     P13 = computeLength(first, third)
-    P23 = computeLength(second, third)  
-    
+    P23 = computeLength(second, third)
     a = math.pow(P12,2)
     b = math.pow(P13,2)
     c = math.pow(P23,2)
     d = (a + b - c) / (2 * P12 * P13)
-    print(d)
     angle = np.arccos( d )
     return angle
     
@@ -39,20 +32,18 @@ def findAngles(shape):
         angle = computeAngle(first,second,third)
         result.append(angle)
         i += 1
-    
     first = shape[i] 
     second = shape[i+1]
     third = shape[1]
     angle = computeAngle(first,second,third)
     result.append(angle)
-    
     return result
 
 def main():
     from numpy import genfromtxt
     shape = genfromtxt('../Shapes/Shape1.csv', delimiter=',')
     angles = findAngles(shape)
-    print(angles)
+    return angles
 
 if (__name__ == "__main__"):
     main()
