e9b67f3c89673a5128017bec1ec663caf3650c9f
rkans02 <rkans02@users.noreply.github.com>
2019-06-19 14:34:00 +0200

    Created and implemented Angle finding
    Not using arccos yes

---
+++ Scripts/AngleFinder.py
@@ -0,0 +1,52 @@
+import sys
+import math
+import os, random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def computeLength(P1, P2):    
+    x1 = P1[0]
+    y1 = P1[1]
+    x2 = P2[0]
+    y2 = P2[1]
+    
+    a = math.pow( (x1 - x2) , 2 )
+    b = math.pow( (y1 - y2) , 2 )
+    
+    length = math.sqrt( a + b )
+    return length
+
+def computeAngle(first, second, third):
+    P12 = computeLength(first, second)
+    P13 = computeLength(first, third)
+    P23 = computeLength(second, third)  
+    
+    a = math.pow(P12,2)
+    b = math.pow(P13,2)
+    c = math.pow(P23,2)
+    d = (a + b + c) / (2 * P12 * P13)
+    print(d)
+    angle = np.cos( d )
+    return angle
+    
+def findAngles(shape):
+    result = []
+    i = 0
+    while i < len(shape) - 3:
+        first = shape[i] 
+        second = shape[i+1]
+        third = shape[i+2]
+        angle = computeAngle(first,second,third)
+        result.append(angle)
+        i += 1
+    return result
+
+def main():
+    from numpy import genfromtxt
+    shape = genfromtxt('../Shapes/Shape1.csv', delimiter=',')
+    angles = findAngles(shape)
+    print(angles)
+
+if (__name__ == "__main__"):
+    main()
+
